2949b0dbf19396a8d54e78493ea85ffb6b227a67
Innocent <innocentsagini16@gmail.com>
2023-04-04 17:19:56 -0700

    7-print_chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_chessboard - A function that prints out a chessboard
+ *
+ *@a - The 8 column chessboard to be printed
+ *
+ */
+
+void print_chessboard(char (*a)[8])
+
+{
+	int fanC = 0;
+	int lenC = 0;
+	
+
+	for (; lenC < 8 ;)
+	{
+		fanC = 0;
+		for (; fanC < 8 ;)
+			putchar (a[lenC][fanC]);
+		fanC++;
+		putchar ('\n');
+		lenC++;	
+	}
+	break;
+}
+
