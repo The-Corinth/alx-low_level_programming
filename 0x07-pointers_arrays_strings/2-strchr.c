27e1703f74a07e77145305411fc9b019152d9ad2
Innocent <innocentsagini16@gmail.com>
2023-04-04 18:25:06 -0700

    2-strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <string.h>
 /**
  * _strchr - Locates the first occurrence of c in s.
  *
@@ -12,13 +14,10 @@ char *_strchr(char *s, char c)
 
 	k = 0;
 
-	for (; *s != '\0' ;)
-	{
-		if (*s == c)
-			return (s);
-	else if (*(s + 1) == c)
-		return (s + 1);
+	for (; *(s + k) ;)
+
+		if (*(s + k) == c)
+			return (s + k);
 	k++;
-	{
-	return (0);
+	return (NULL);
 }
