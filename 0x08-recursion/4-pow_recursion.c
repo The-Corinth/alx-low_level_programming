e2775360c85c76dd62fdd829a3458f9abd9e6c02
Innocent <innocentsagini16@gmail.com>
2023-04-06 19:40:50 -0700

    4-pow

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * _pow_recursion - Returns the value of x raised to power y
- * @y : The power number
- * @x : x ^ y
- * Return : The result of power
+ * @y: The power number
+ * @x: x ^ y
+ * Return: The result of power
  */
 int _pow_recursion(int x, int y)
 {
