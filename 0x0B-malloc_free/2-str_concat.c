5e612fbf81eb04ce8e3ea8d4efb735192f8cc472
Innocent <innocentsagini16@gmail.com>
2023-04-11 18:40:59 -0700

    c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,10 +12,12 @@ char *str_concat(char *s1, char *s2)
 	char *str_concat;
 	size_t length_s1 = strlen(s1);
 	size_t length_s2 = strlen(s2);
-	
+
 	str_concat = malloc(length_s1 + length_s2 + 1);
-	if (!s1)s1 = "Betty";
-	if (!s2)s2 = "Holberton";
+	if (!s1)
+		s1 = "Betty";
+	if (!s2)
+		s2 = "Holberton";
 	if (!str_concat)
 		return (NULL);
 	strcpy(str_concat, s1);
