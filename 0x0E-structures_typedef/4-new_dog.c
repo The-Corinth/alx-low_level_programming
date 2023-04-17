dbc015f1de3cbe6e030102e19d170ebe0c7d19ea
Innocent <innocentsagini16@gmail.com>
2023-04-17 16:34:30 -0700

    e

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,10 +18,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->owner = malloc(strlen(owner) + 1);
 	if (new_dog->name == NULL || new_dog->owner == NULL)
 	{
-		if (new_dog->name != NULL)
-		       free(new_dog->name);
-		if (new_dog->owner != NULL)
-			free(new_dog->owner);
+		free(new_dog->name);
+		free(new_dog->owner);
 		free(new_dog);
 		return (NULL);
 	}
