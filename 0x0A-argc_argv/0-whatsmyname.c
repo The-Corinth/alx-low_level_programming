d47466a0c268daa4077cd952d4cb819a15334944
Innocent <innocentsagini16@gmail.com>
2023-04-22 17:20:52 -0700

    myname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: Number of commands
+ * @argv: arguments entered
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc-1]);
+	return (0);
+}
