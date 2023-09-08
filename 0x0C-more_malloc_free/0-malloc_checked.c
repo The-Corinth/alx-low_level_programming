f86bdfde87b28060a73401259df94b93c9b09570
Innocent <innocentsagini16@gmail.com>
2023-09-07 17:06:29 -0700

    Malloc1

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)/* error checking */
+		exit(98);/* progam exits with this error code */
+
+	return (ptr);
+}
