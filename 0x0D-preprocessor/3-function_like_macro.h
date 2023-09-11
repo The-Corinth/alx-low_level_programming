1abb79d57301ce7b29e897b691d04d538cc03301
Innocent <innocentsagini16@gmail.com>
2023-09-11 03:57:12 -0700

    3-function

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * ABS - Computes the absolute value of a number.
+ * @x: The number for which to compute the absolute value.
+ *
+ * Return: The absolute value of x.
+ */
+#define ABS(x) ((x) < (0) ? -(x) : (x))
+
+#endif /* FUNCTION_LIKE_MACRO_H */
