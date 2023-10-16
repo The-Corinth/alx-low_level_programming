b873cadfc2b2426f1c0fef9edc09b5d50be5e19b
Innocent <innocentsagini16@gmail.com>
2023-10-16 09:07:44 -0700

    5-flip

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -14,12 +14,11 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int current;
 	unsigned long int exclusive = n ^ m;
 
-	for (a = 63; a >= 0; a--)
-/*Iterate through each bit position in 'exclusive'
- * from the most significant bit to the least significant bit.
- * If the current bit is set (1),
- * increment the 'countbit' to count differing bits.
- */
+	for (a = 63; a >= 0; a--)/*Iterate through each bit position in 'exclusive'
+				   *from the most significant bit to the least significant bit.
+				   *If the current bit is set (1),
+				   *increment the 'countbit' to count differing bits.
+				   */
 	{
 		current = exclusive >> a;
 		if (current & 1)
